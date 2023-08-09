26daf9c04d5209310bf0f6b5712bd9982ad9454b
marco antonio <valeriomartinezmarco@gmail.com>
2023-08-08 23:07:49 -0600

    08082023

--- app/Views/templeate/footer.php
+++ app/Views/templeate/footer.php
@@ -22,14 +22,14 @@
   </div>
 
   <!-- Vendor JS Files -->
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="<?= base_url()?>vendor/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?= base_url()?>vendor/assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="<?= base_url()?>vendor/assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="<?= base_url()?>vendor/assets/vendor/aos/aos.js"></script>
+  <script src="<?= base_url()?>vendor/assets/vendor/php-email-form/validate.js"></script>
 
   <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
+  <script src="<?= base_url()?>vendor/assets/js/main.js"></script>
 
 </body>
 
