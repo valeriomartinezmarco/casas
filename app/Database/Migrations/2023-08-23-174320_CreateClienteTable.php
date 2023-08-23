f2ef04509770ec2afa67678e539f728876a39c49
Marco Antonio Valerio Martínez <marco.valeriom@incmnsz.com>
2023-08-23 12:54:20 -0600

    migrateCasas

--- app/Database/Migrations/2023-08-23-174320_CreateClienteTable.php
+++ app/Database/Migrations/2023-08-23-174320_CreateClienteTable.php
@@ -19,6 +19,25 @@ class CreateClienteTable extends Migration
         ]);
         $this->forge->addKey('id', true);
         $this->forge->createTable('cliente'); 
+
+
+        $this->forge->addField([
+            'id' => ['type' => 'INT','usigned'=>true, 'constraint' => 5],
+            'tituloCasa' => ['type' => 'VARCHAR', 'constraint' => 200],
+            'introCasa' => ['type' => 'VARCHAR', 'constraint' => 200],
+            'botonCasaCel' => ['type' => 'VARCHAR', 'constraint' => 200],
+            'tituloDescCasa' => ['type' => 'VARCHAR', 'constraint' => 200],
+            'descCasa' => ['type' => 'VARCHAR', 'constraint' => 200],
+            'descJardin' => ['type' => 'VARCHAR', 'constraint' => 200],
+            'footerCasa' => ['type' => 'VARCHAR', 'constraint' => 200],
+            'infoCasa' => ['type' => 'VARCHAR', 'constraint' => 200],
+            'categoriaCasa' => ['type' => 'VARCHAR', 'constraint' => 200],
+            'adminCasa' => ['type' => 'VARCHAR', 'constraint' => 200],
+            'fotosfechaCasa' => ['type' => 'VARCHAR', 'constraint' => 200],
+            'ligafotosCasa' => ['type' => 'VARCHAR', 'constraint' => 200],
+            
+        ]);
+        $this->forge->createTable('casas'); 
     }
 
     public function down()
