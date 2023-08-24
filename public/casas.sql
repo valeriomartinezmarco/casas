03c27c4a4510fa3b2117960e9cf8ff9e7c9de14a
Marco Valerio <valerionartinezmarco@gmail.com>
2023-08-24 13:52:28 -0600

    tagCasa2

--- public/casas.sql
+++ public/casas.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-08-2023 a las 07:50:19
--- Versión del servidor: 10.4.27-MariaDB
--- Versión de PHP: 8.0.25
+-- Tiempo de generación: 24-08-2023 a las 21:51:37
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,6 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `casas` (
   `id` int(5) NOT NULL,
   `tituloCasa` text NOT NULL,
+  `tagCasa` text NOT NULL,
   `introCasa` text NOT NULL,
   `botonCasaCel` text NOT NULL,
   `tituloDescCasa` text NOT NULL,
@@ -50,8 +51,8 @@ CREATE TABLE `casas` (
 -- Volcado de datos para la tabla `casas`
 --
 
-INSERT INTO `casas` (`id`, `tituloCasa`, `tagCasa`,`introCasa`, `botonCasaCel`, `tituloDescCasa`, `descCasa`, `descJardin`, `footerCasa`, `infoCasa`, `categoriaCasa`, `adminCasa`, `fotosfechaCasa`, `ligafotosCasa`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(0, 'Casa Naranja', 'casa_naranja','<p style=\"text-align:center\">Disfruta de la una quinta casa privada en Chinconcuac, Xochitepec.<br />\r\nEl costo es para 7 personas y la capacidad m&aacute;xima es de 10.<br />\r\nDeseas reservar, lo puedes hacer con $1000 y el resto al estar instalado dentro de la casa.</p>', 'Me interesa, deseo mas información de la <i>Casa Naranja</i> y fechas disponibles.', 'bbbb', '88hkjh', '9jjkbjk bjkbjk', 'jkhkj jk', 'kj 9877 h j', '8897 hhh', 'hhj877 ', '87hjh jh jh', 'jh s', '2023-08-23 23:24:36', '2023-08-23 23:24:36', '2023-08-23 23:24:36');
+INSERT INTO `casas` (`id`, `tituloCasa`, `tagCasa`, `introCasa`, `botonCasaCel`, `tituloDescCasa`, `descCasa`, `descJardin`, `footerCasa`, `infoCasa`, `categoriaCasa`, `adminCasa`, `fotosfechaCasa`, `ligafotosCasa`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(0, 'Casa Naranja', 'casa_naranja', '<p style=\"text-align:center\">Disfruta de la una quinta casa privada en Chinconcuac, Xochitepec.<br />\r\nEl costo es para 7 personas y la capacidad m&aacute;xima es de 10.<br />\r\nDeseas reservar, lo puedes hacer con $1000 y el resto al estar instalado dentro de la casa.</p>', 'Me interesa, deseo mas información de la <i>Casa Naranja</i> y fechas disponibles.', '<p>&nbsp;</p>\r\n\r\n<p style=\"text-align:justify\"><em>Ubicada en Chinconcuac Xochitepec, casa privada con c&aacute;maras de seguridad, cerca elecrificada para su seguridad. &nbsp;Cuenta con alberca, Jacuzzi, camastros, regadera, ba&ntilde;o en area verde,palapa con ventilador, mesa en palapa, iluminaci&oacute;n en palapa, area de asador con tarja. &nbsp;</em></p>\r\n', '<p>&nbsp;</p>\n\n<p style=\"text-align:justify\"><u><em><strong>Casa para 7 personas (M&aacute;ximo 10 personas)</strong></em></u></p>\n\n<ul>\n	<li style=\"text-align: justify;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Capacidad para 4 autos</li>\n	<li style=\"text-align: justify;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Se aceptan mascotas sin costo</li>\n	<li style=\"text-align: justify;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; La casa no tiene capacidad para eventos&lt;br&gt;</li>\n	<li style=\"text-align: justify;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Hora de entrada desde 9 am&nbsp; y salida 2 pm (**Se puede ampliar hora salida)</li>\n	<li style=\"text-align: justify;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Cocina cuenta (Cafetera,Estufa,Refrigerador,Sartens,Platos,Vasos)</li>\n	<li style=\"text-align: justify;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Incluye un Garrafon nuevo</li>\n	<li style=\"text-align: justify;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Incluye Un rollo nuevo por cada Ba&ntilde;o</li>\n	<li style=\"text-align: justify;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Incluye Bolsas de Basura</li>\n	<li style=\"text-align: justify;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; No cuenta con Horno de Microondas</li>\n	<li style=\"text-align: justify;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; No incluye Toallas de Ba&ntilde;o</li>\n	<li style=\"text-align: justify;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Cuenta con un cuarto Privado, con Ba&ntilde;o.</li>\n	<li style=\"text-align: justify;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; En la parte superior hay dos camas matrimoniales con sofa cama, con ba&ntilde;o completo.</li>\n</ul>\n', '<p>&nbsp;</p>\n\n<p style=\"text-align:justify\"><u><em><strong>Casa para 7 personas (M&aacute;ximo 10 personas)</strong></em></u></p>\n\n<ul>\n	<li style=\"text-align: justify;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Capacidad para 4 autos</li>\n	<li style=\"text-align: justify;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Se aceptan mascotas sin costo</li>\n	<li style=\"text-align: justify;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; La casa no tiene capacidad para eventos&lt;br&gt;</li>\n	<li style=\"text-align: justify;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Hora de entrada desde 9 am&nbsp; y salida 2 pm (**Se puede ampliar hora salida)</li>\n	<li style=\"text-align: justify;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Cocina cuenta (Cafetera,Estufa,Refrigerador,Sartens,Platos,Vasos)</li>\n	<li style=\"text-align: justify;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Incluye un Garrafon nuevo</li>\n	<li style=\"text-align: justify;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Incluye Un rollo nuevo por cada Ba&ntilde;o</li>\n	<li style=\"text-align: justify;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Incluye Bolsas de Basura</li>\n	<li style=\"text-align: justify;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; No cuenta con Horno de Microondas</li>\n	<li style=\"text-align: justify;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; No incluye Toallas de Ba&ntilde;o</li>\n	<li style=\"text-align: justify;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Cuenta con un cuarto Privado, con Ba&ntilde;o.</li>\n	<li style=\"text-align: justify;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; En la parte superior hay dos camas matrimoniales con sofa cama, con ba&ntilde;o completo.</li>\n</ul>\n', 'jkhkj jk', 'kj 9877 h j', '8897 hhh', 'hhj877 ', '87hjh jh jh', 'jh s', '2023-08-23 23:24:36', '2023-08-23 23:24:36', '2023-08-23 23:24:36');
 
 --
 -- Índices para tablas volcadas
@@ -61,7 +62,8 @@ INSERT INTO `casas` (`id`, `tituloCasa`, `tagCasa`,`introCasa`, `botonCasaCel`, 
 -- Indices de la tabla `casas`
 --
 ALTER TABLE `casas`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
