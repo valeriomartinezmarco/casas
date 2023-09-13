a72192c7f09054cf85dabb3ebd22159763a57c05
marco antonio <valeriomartinezmarco@gmail.com>
2023-09-13 00:15:48 -0600

    reservar

--- app/Views/nosotros.php
+++ app/Views/nosotros.php
@@ -1,16 +1,15 @@
 <?php
 include_once('template/header.php');
 ?>
-<style>
+
 
 <div class="page-header d-flex align-items-center">
       <div class="container position-relative">
         <div class="row d-flex justify-content-center">
           <div class="col-lg-6 text-center">
-            <h2>About</h2>
-            <p>Odio et unde deleniti. Deserunt numquam exercitationem. Officiis quo odio sint voluptas consequatur ut a odio voluptatem. Sit dolorum debitis veritatis natus dolores. Quasi ratione sint. Sit quaerat ipsum dolorem.</p>
+            <h2>Reservaciones.</h2>
+            <p>Que se requiere para reservanos.</p>
 
-            <a class="cta-btn" href="contact.html">Available for hire</a>
 
           </div>
         </div>
@@ -23,39 +22,24 @@ include_once('template/header.php');
 
         <div class="row gy-4 justify-content-center">
           <div class="col-lg-4">
-            <img src="assets/img/profile-img.jpg" class="img-fluid" alt="">
+            <img src="<?= base_url()?>vendor/assets/img/gallery/casa_jazam.jpeg" class="img-fluid" alt="">
           </div>
           <div class="col-lg-5 content">
-            <h2>Professional Photographer from New York</h2>
+            <h2>Consideraciones importantes</h2>
             <p class="fst-italic py-3">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-              magna aliqua.
+              Cada casa considera la cantidad para reservación.
             </p>
-            <div class="row">
-              <div class="col-lg-6">
-                <ul>
-                  <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>1 May 1995</span></li>
-                  <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong> <span>www.example.com</span></li>
-                  <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+123 456 7890</span></li>
-                  <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>New York, USA</span></li>
-                </ul>
-              </div>
-              <div class="col-lg-6">
-                <ul>
-                  <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>30</span></li>
-                  <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Master</span></li>
-                  <li><i class="bi bi-chevron-right"></i> <strong>PhEmailone:</strong> <span>email@example.com</span></li>
-                  <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
-                </ul>
-              </div>
-            </div>
+            <p class="fst-italic py-3">
+              El mensaje en la transferencia debera ser de la siguiente manera:
+              <br>diamesano casa, ejemplo: 13092023 naranja
+            </p>
+            <p class="fst-italic py-3">
+              Enviar la ficha de pago para concretar la reservación. Podrás realizar abonos antes de la fecha de tu visita o en el día de la entrega de la casa , una vez instalado podrás realizar el pago total de la casa.
+            </p>
+            
             <p class="py-3">
-              Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis.
-              Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque. Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.
-            </p>
-            <p class="m-0">
-              Recusandae est praesentium consequatur eos voluptatem. Vitae dolores aliquam itaque odio nihil. Neque ut neque ut quae voluptas. Maxime corporis aut ut ipsum consequatur. Repudiandae sunt sequi minus qui et. Doloribus molestiae officiis.
-              Soluta eligendi fugiat omnis enim. Numquam alias sint possimus eveniet ad. Ratione in earum eum magni totam.
+              Antes de realizar un pago primero se debe solicitar la confirmación de disponibilidad.</p>
+            <p class="m-0">Debes considerar un depósito en garantía y que a la entrega de la casa se revisa y se hace la devolución. Hay casas que son devolución inmediata y otras que debes esperar hasta 24 hrs.
             </p>
           </div>
         </div>
@@ -68,8 +52,8 @@ include_once('template/header.php');
       <div class="container">
 
         <div class="section-header">
-          <h2>Testimonials</h2>
-          <p>What they are saying</p>
+          <h2>Proceso de reservación</h2>
+          <p>Qué debo hacer para reservar!!.</p>
         </div>
 
         <div class="slides-3 swiper">
