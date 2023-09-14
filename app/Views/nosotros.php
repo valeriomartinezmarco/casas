e19a098efbd00f0da82fb1a829a14e15895555e5
Marco Valerio <valerionartinezmarco@gmail.com>
2023-09-14 14:23:06 -0600

    viewfina45

--- app/Views/nosotros.php
+++ app/Views/nosotros.php
@@ -4,56 +4,18 @@ include_once('template/header.php');
 
 
 <div class="page-header d-flex align-items-center">
-      <div class="container position-relative">
-        <div class="row d-flex justify-content-center">
-          <div class="col-lg-6 text-center">
-            <h2>Reservaciones.</h2>
-            <p>Que se requiere para reservanos.</p>
+</div>
 
-
-          </div>
-        </div>
-      </div>
-    </div><!-- End Page Header -->
 
     <!-- ======= About Section ======= -->
-    <section id="about" class="about">
-      <div class="container">
-
-        <div class="row gy-4 justify-content-center">
-          <div class="col-lg-4">
-            <img src="<?= base_url()?>vendor/assets/img/gallery/casa_jazam.jpeg" class="img-fluid" alt="">
-          </div>
-          <div class="col-lg-5 content">
-            <h2>Consideraciones importantes</h2>
-            <p class="fst-italic py-3">
-              Cada casa considera la cantidad para reservación.
-            </p>
-            <p class="fst-italic py-3">
-              El mensaje en la transferencia debera ser de la siguiente manera:
-              <br>diamesano casa, ejemplo: 13092023 naranja
-            </p>
-            <p class="fst-italic py-3">
-              Enviar la ficha de pago para concretar la reservación. Podrás realizar abonos antes de la fecha de tu visita o en el día de la entrega de la casa , una vez instalado podrás realizar el pago total de la casa.
-            </p>
-            
-            <p class="py-3">
-              Antes de realizar un pago primero se debe solicitar la confirmación de disponibilidad.</p>
-            <p class="m-0">Debes considerar un depósito en garantía y que a la entrega de la casa se revisa y se hace la devolución. Hay casas que son devolución inmediata y otras que debes esperar hasta 24 hrs.
-            </p>
-          </div>
-        </div>
-
-      </div>
-    </section><!-- End About Section -->
-
+    
     <!-- ======= Testimonials Section ======= -->
     <section id="testimonials" class="testimonials">
       <div class="container">
 
         <div class="section-header">
-          <h2>Proceso de reservación</h2>
-          <p>Qué debo hacer para reservar!!.</p>
+          <h3>Proceso de reservación</h3>
+          
         </div>
 
         <div class="slides-3 swiper">
@@ -65,12 +27,12 @@ include_once('template/header.php');
                   <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
                 </div>
                 <p>
-                  Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
+                  Algunas casas aceptan mascotas. (Algunos con costo adicional).
                 </p>
                 <div class="profile mt-auto">
                   <img src="assets/img/testimonials/testimonials-1.jpg" class="testimonial-img" alt="">
-                  <h3>Saul Goodman</h3>
-                  <h4>Ceo &amp; Founder</h4>
+                  <h3>Casas de descanso</h3>
+                  <h4></h4>
                 </div>
               </div>
             </div><!-- End testimonial item -->
@@ -81,12 +43,12 @@ include_once('template/header.php');
                   <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
                 </div>
                 <p>
-                  Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
+                Para reservar se requiere un anticipo.
                 </p>
                 <div class="profile mt-auto">
                   <img src="assets/img/testimonials/testimonials-2.jpg" class="testimonial-img" alt="">
-                  <h3>Sara Wilsson</h3>
-                  <h4>Designer</h4>
+                  <h3>Casas de descanso</h3>
+                  <h4></h4>
                 </div>
               </div>
             </div><!-- End testimonial item -->
@@ -97,12 +59,13 @@ include_once('template/header.php');
                   <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
                 </div>
                 <p>
-                  Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.
+                El mensaje en la transferencia debera ser de la siguiente manera:
+              <br>diamesano casa, ejemplo: 13092023 naranja
                 </p>
                 <div class="profile mt-auto">
                   <img src="assets/img/testimonials/testimonials-3.jpg" class="testimonial-img" alt="">
-                  <h3>Jena Karlis</h3>
-                  <h4>Store Owner</h4>
+                  <h3>Casas de descanso</h3>
+                  <h4></h4>
                 </div>
               </div>
             </div><!-- End testimonial item -->
@@ -113,12 +76,12 @@ include_once('template/header.php');
                   <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
                 </div>
                 <p>
-                  Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
+                Enviar la ficha de pago para concretar la reservación. Podrás realizar abonos antes de la fecha de tu visita o en el día de la entrega de la casa , una vez instalado podrás realizar el pago total de la casa.
                 </p>
                 <div class="profile mt-auto">
                   <img src="assets/img/testimonials/testimonials-4.jpg" class="testimonial-img" alt="">
-                  <h3>Matt Brandon</h3>
-                  <h4>Freelancer</h4>
+                  <h3>Casas de descanso</h3>
+                  <h4></h4>
                 </div>
               </div>
             </div><!-- End testimonial item -->
@@ -129,12 +92,14 @@ include_once('template/header.php');
                   <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
                 </div>
                 <p>
-                  Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
+                Antes de realizar un pago primero se debe solicitar la confirmación de disponibilidad.</p>
+            <p class="m-0">Debes considerar un depósito en garantía y que a la entrega de la casa se revisa y se hace la devolución. Hay casas que son devolución inmediata y otras que debes esperar hasta 24 hrs.
+            </p>
                 </p>
                 <div class="profile mt-auto">
                   <img src="assets/img/testimonials/testimonials-5.jpg" class="testimonial-img" alt="">
-                  <h3>John Larson</h3>
-                  <h4>Entrepreneur</h4>
+                  <h3>Casas de descanso</h3>
+                  <h4></h4>
                 </div>
               </div>
             </div><!-- End testimonial item -->
